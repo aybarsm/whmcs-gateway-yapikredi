6490c4b3159732e5677af0c3b7b7d5a2cc8a8945
Murat Aybars <hello@aybarsm.com>
2023-10-06 18:05:19 +0100

    Path update

--- module.php
+++ module.php
@@ -1,10 +1,10 @@
 <?php
 
 $vendorAutoload = "require_once implode(DIRECTORY_SEPARATOR, [__DIR__, basename(__FILE__, '.php'), 'vendor', 'autoload.php']);";
-$configFilePath = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'yapikredi.php';
+$configFilePath = dirname(__DIR__, 4) . DIRECTORY_SEPARATOR . 'yapikredi.php';
 $configFileContent = "<?php \n\n{$vendorAutoload}\n\n require_once implode(DIRECTORY_SEPARATOR, [__DIR__, basename(__FILE__, '.php'), 'vendor', 'aybarsm', 'whmcs-gateway-yapikredi', 'functions', 'whmcs.module.config.php']);";
 $configFileExists = file_exists($configFilePath);
-$callbackFilePath = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'callback' . DIRECTORY_SEPARATOR . 'yapikredi.php';
+$callbackFilePath = dirname(__DIR__, 4) . DIRECTORY_SEPARATOR . 'callback' . DIRECTORY_SEPARATOR . 'yapikredi.php';
 $callbackFileContent = "<?php \n\n{$vendorAutoload}\n\n require_once implode(DIRECTORY_SEPARATOR, [dirname(__DIR__), basename(__FILE__, '.php'), 'vendor', 'aybarsm', 'whmcs-gateway-yapikredi', 'functions', 'whmcs.module.callback.php']);";
 $callbackFileExists = file_exists($callbackFilePath);
 function consoleMessage(string $fileType, string $status, string $path){

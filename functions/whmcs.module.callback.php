a0382727111d6dbbc213cf1ae28f8e55b870dd60
Murat Aybars <hello@aybarsm.com>
2023-10-06 18:10:48 +0100

    Path update

--- functions/whmcs.module.callback.php
+++ functions/whmcs.module.callback.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once dirname(__DIR__,6) . DIRECTORY_SEPARATOR . 'init.php';
+require_once dirname(__DIR__,7) . DIRECTORY_SEPARATOR . 'init.php';
 
 Aybarsm\Whmcs\Gateway\Yapikredi\Posnet::paymentCallback();
